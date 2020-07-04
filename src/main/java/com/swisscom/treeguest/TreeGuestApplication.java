d3beca77d11182976621c6e79ee9de4805a434d6
vteixeira <vteixeira@bol.com>
2020-07-04 07:31:44 +0200

    fist commit

---
+++ src/main/java/com/swisscom/treeguest/TreeGuestApplication.java
@@ -0,0 +1,13 @@
+package com.swisscom.treeguest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeGuestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TreeGuestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/swisscom/treeguest/TreeGuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swisscom.treeguest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeGuestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
