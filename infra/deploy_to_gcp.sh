c2c858030f70aa287bc9f118b69a7e862cee7005
vteixeira <vteixeira@bol.com>
2020-08-12 08:14:54 +0200

    change the service to fit with Istio pattern

---
+++ infra/deploy_to_gcp.sh
@@ -0,0 +1,8 @@
+cd /tree_guest/k8s/
+kustomize edit set image ${PROJECT_IMAGE}$CIRCLE_WORKFLOW_ID
+cat kustomization.yml
+kustomize build > ./kustomized-deployment.yaml
+cat ./kustomized-deployment.yaml
+istioctl kube-inject -f ./kustomized-deployment.yaml > istio-deployment.yaml   # convert kubernates deployment to istio;
+kubectl apply -f ./istio-deployment.yaml
+kubectl rollout status deployment/tree-quest-backend
